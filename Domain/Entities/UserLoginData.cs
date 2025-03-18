daca82b22ea49c27750df86f5f9d5c453a900e49
shotiko akhlouri <shota.akhlouri@gmail.com>
2025-03-18 23:46:32 +0400

    removed entity email address attribute validation

--- Domain/Entities/UserLoginData.cs
+++ Domain/Entities/UserLoginData.cs
@@ -5,7 +5,6 @@ namespace Domain.Entities
     public class UserLoginData
     {
         public int ID { get; set; }
-        [EmailAddress]
         public string Email { get; set; }
         public byte [] PasswordHash { get; set; }
         public byte [] PasswordSalt { get; set; }
