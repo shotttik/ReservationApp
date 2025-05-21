2e7327ca0738c1bca134c7d6abc76883cef6f5d4
shotiko akhlouri <shota.akhlouri@gmail.com>
2025-05-21 11:56:52 +0400

    enabled Swagger UI to support JWT Bearer token authentication

--- API/Program.cs
+++ API/Program.cs
@@ -87,8 +87,33 @@ void ConfigureServices(IServiceCollection services)
 
     // Add Swagger/OpenAPI services
     services.AddEndpointsApiExplorer();
-    services.AddSwaggerGen();
+    services.AddSwaggerGen(options =>
+    {
+        options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT",
+            In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+            Description = "Enter your valid JWT token."
+        });
 
+        options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+    });
     // Register DbContext with SQL Server
     services.AddStackExchangeRedisCache(options =>
     {
