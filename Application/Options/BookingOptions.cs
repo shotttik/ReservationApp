b0b82daacbae630154baa619c5444a6d72c77e1b
shotiko akhlouri <shota.akhlouri@gmail.com>
2026-07-09 16:10:41 +0400

    fixed appsettings.docker.json missed bookingsettings.

--- Application/Options/BookingOptions.cs
+++ Application/Options/BookingOptions.cs
@@ -1,17 +1,24 @@
-﻿namespace Application.Options
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Options
 {
     public class BookingOptions
     {
         public const string ConfigurationSection = "BookingSettings";
+        [Required]
         public int VerificationCodeLength { get; set; }
+        [Required]
         public int VerificationCodeExpirationMinutes { get; set; }
 
+        [Required]
         public GuestToken GuestToken { get; set; } = null!;
     }
 
     public class GuestToken
     {
+        [Required]
         public string Key { get; set; } = null!;
+        [Required]
         public int ExpirationMinutes { get; set; }
     }
 }
